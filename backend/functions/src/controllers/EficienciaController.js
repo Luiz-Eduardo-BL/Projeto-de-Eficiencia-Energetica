1fe9b3b17ab47aafeec0468ab4dd7792768a3d4e
João Lucas Mota <joaomota59@hotmail.com>
2022-08-03 16:59:27 -0300

    add referencia a tabela de historico a partir da tabela de resultado da eficiencia

--- backend/functions/src/controllers/EficienciaController.js
+++ backend/functions/src/controllers/EficienciaController.js
@@ -1,4 +1,6 @@
 import db from '../database/index.js'
+import moment from 'moment-timezone'
+
 
 export default {
 
@@ -6,109 +8,137 @@ export default {
         response.set('Access-Control-Allow-Origin', '*')
 
         await db.collection('eficiencia')
-        .orderBy('indiceK', 'asc').get()
-        .then(snapshot => {
-            let eficiencia = []
-            snapshot.forEach(doc => {
-                eficiencia.push({
-                    indiceK: doc.data().indiceK,
-                    nivelA: doc.data().nivelA,
-                    nivelB: doc.data().nivelB,
-                    nivelC: doc.data().nivelC,
-                    nivelD: doc.data().nivelD
-                    
+            .orderBy('indiceK', 'asc').get()
+            .then(snapshot => {
+                let eficiencia = []
+                snapshot.forEach(doc => {
+                    eficiencia.push({
+                        indiceK: doc.data().indiceK,
+                        nivelA: doc.data().nivelA,
+                        nivelB: doc.data().nivelB,
+                        nivelC: doc.data().nivelC,
+                        nivelD: doc.data().nivelD
+
+                    })
                 })
+                return response.json(eficiencia)
             })
-            return response.json(eficiencia)
-        })
-        .catch((err) => {
-            response.status(404).json({error: err})
-        })
+            .catch((err) => {
+                response.status(404).json({ error: err })
+            })
     },
 
     async result(request, response) {
         response.set('Access-Control-Allow-Origin', '*')
 
-        const {largura, comprimento,distanciaPlanoDeTrabalhoTeto,potenciaLampada,qntdLampadas,iluminanciaMediaFinal, aparelho} = request.body
+        let dataLeitura = moment.tz("America/Sao_Paulo").format("DD-MM-YYYY") //data atual em que ocorreu a leitura na sala
+
+        //console.log(dataAtual)
+
+        const { sala, largura, comprimento, distanciaPlanoDeTrabalhoTeto, potenciaLampada, qntdLampadas, iluminanciaMediaFinal, aparelho } = request.body
 
         const area = largura * comprimento
         const potenciaTotal = potenciaLampada * qntdLampadas
-        const indiceK = area/(distanciaPlanoDeTrabalhoTeto*(comprimento+largura))
+        const indiceK = area / (distanciaPlanoDeTrabalhoTeto * (comprimento + largura))
+        let classificacao = ""
 
         //if(aparelho.toLowerCase().includes("samsung")){
 
-       // }
+        // }
 
         await db.collection('eficiencia')
-        .orderBy('indiceK', 'asc').get()
-        .then(snapshot => {
-            let difAux = 0
-            let eficienciaAux = []
-            let countAux = 0
-            let pos = 0
-            snapshot.forEach(doc => {
-                eficienciaAux.push({
-                    nivelA: doc.data().nivelA,
-                    nivelB: doc.data().nivelB,
-                    nivelC: doc.data().nivelC,
-                    nivelD: doc.data().nivelD
+            .orderBy('indiceK', 'asc').get()
+            .then(async snapshot => {
+                let difAux = 0
+                let eficienciaAux = []
+                let countAux = 0
+                let pos = 0
+                snapshot.forEach(doc => {
+                    eficienciaAux.push({
+                        nivelA: doc.data().nivelA,
+                        nivelB: doc.data().nivelB,
+                        nivelC: doc.data().nivelC,
+                        nivelD: doc.data().nivelD
+                    })
+                    if (countAux == 0)
+                        difAux = Math.abs(doc.data().indiceK - indiceK)
+                    else if (Math.abs(doc.data().indiceK - indiceK) < difAux) {
+                        difAux = Math.abs(doc.data().indiceK - indiceK)
+                        pos = countAux //posição do nivel k mais próximo
+                    }
+
+                    countAux++
                 })
-                if (countAux == 0)
-                    difAux = Math.abs(doc.data().indiceK - indiceK)
-                else if(Math.abs(doc.data().indiceK - indiceK) < difAux) {
-                    difAux = Math.abs(doc.data().indiceK - indiceK)
-                    pos = countAux //posição do nivel k mais próximo
+
+
+                let eficiencia = eficienciaAux[pos]
+
+                let densidadePotIluminacaoRelativa = potenciaTotal * 100 / (area * iluminanciaMediaFinal)
+
+                if (densidadePotIluminacaoRelativa <= eficiencia.nivelA) {
+                    classificacao = "A"
+                }
+                else if (densidadePotIluminacaoRelativa <= eficiencia.nivelB) {
+                    classificacao = "B"
+                }
+                else if (densidadePotIluminacaoRelativa <= eficiencia.nivelC) {
+                    classificacao = "C"
+                }
+                else if (densidadePotIluminacaoRelativa <= eficiencia.nivelD) {
+                    classificacao = "D"
+                }
+                else {
+                    classificacao = "E"
                 }
 
-                countAux++
+                
+                await db.collection('historico')
+                    .where('sala', "==", sala)
+                    .where('dataLeitura',"==", dataLeitura)
+                    .get()
+                    .then(async snapshot => {
+                        
+                        if (snapshot.empty) {
+                            
+                            await db.collection('historico').add({
+                                sala,
+                                iluminanciaMediaFinal,
+                                classificacao,
+                                indiceK,
+                                densidadePotIluminacaoRelativa,
+                                aparelho,
+                                dataLeitura
+                            }).then(() => {
+                                response.json({ classificacao, indiceK, densidadePotIluminacaoRelativa })
+                            }).catch(err => {
+                                response.status(404).json({ error: err })
+                            })
+                        }
+                        else {
+                                await db.collection('historico')
+                                .doc(snapshot.docs[0].id)//pega o id do documento que tem a data atual (o primeiro da query)
+                                .update({
+                                    iluminanciaMediaFinal,
+                                    classificacao,
+                                    indiceK,
+                                    densidadePotIluminacaoRelativa,
+                                    aparelho,
+                                    dataLeitura
+                                }).then(() => {
+                                    response.json({ classificacao, indiceK, densidadePotIluminacaoRelativa })
+                                }).catch(err => {
+                                    response.status(404).json({ error: err })
+                                })
+
+                        }
+                    }).catch(err => {
+                        response.status(404).json({ error: err })
+                    })
+            })
+            .catch((err) => {
+                response.status(404).json({ error: err })
             })
 
-
-            let eficiencia = eficienciaAux[pos]
-
-            let densidadePotIluminacaoRelativa = potenciaTotal*100/(area*iluminanciaMediaFinal)
-
-            if (densidadePotIluminacaoRelativa <= eficiencia.nivelA) {
-                return response.json({
-                    classificacao: 'A',
-                    indiceK: indiceK,
-                    dpiRf : densidadePotIluminacaoRelativa
-                })
-            }
-            else if (densidadePotIluminacaoRelativa <= eficiencia.nivelB) {
-                return response.json({
-                    classificacao: 'B',
-                    indiceK: indiceK,
-                    dpiRf : densidadePotIluminacaoRelativa
-                })
-            }
-            else if (densidadePotIluminacaoRelativa <= eficiencia.nivelC) {
-                return response.json({
-                    classificacao: 'C',
-                    indiceK: indiceK,
-                    dpiRf : densidadePotIluminacaoRelativa
-                })
-            }
-            else if (densidadePotIluminacaoRelativa <= eficiencia.nivelD) {
-                return response.json({
-                    classificacao: 'D',
-                    indiceK: indiceK,
-                    dpiRf : densidadePotIluminacaoRelativa
-                })
-            }
-            else{
-                return response.json({
-                    classificacao: 'E',
-                    indiceK: indiceK,
-                    dpiRf : densidadePotIluminacaoRelativa
-                })
-            
-            }
-
-
-    })
-    .catch((err) => {
-        response.status(404).json({error: err})
-    })
+                        
     }
 }
