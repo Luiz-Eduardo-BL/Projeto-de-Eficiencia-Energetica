97cd127314dacd5042ba6e720863c46471b8a60e
João Lucas Mota <joaomota59@hotmail.com>
2022-08-07 17:18:29 -0300

    alterações na tela de descricao da sala

--- frontend/LuxMeter/app/src/main/java/moe/feo/luxmeter/RoomDescription.java
+++ frontend/LuxMeter/app/src/main/java/moe/feo/luxmeter/RoomDescription.java
@@ -10,5 +10,9 @@ public class RoomDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_description);
+
+        getSupportActionBar().setTitle("Informações da Sala");
+
+
     }
 }
