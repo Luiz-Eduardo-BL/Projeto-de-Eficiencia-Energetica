7f05f7f79fd4d0d9a0c5ef58a9fbd8cabdaf6fab
João Lucas Mota <joaomota59@hotmail.com>
2022-08-06 23:20:56 -0300

    add projeto inicial do frontend

--- frontend/src/pages/RoomDetails/index.js
+++
@@ -1,9 +0,0 @@
-import {View,Text} from 'react-native'
-
-export default function RoomDetails(){
-    return(
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>Room Details</Text>
-        </View>
-    )
-}
