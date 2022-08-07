a3619b4ad4e7f6910d42871ef7754330b2fc45fd
João Lucas Mota <joaomota59@hotmail.com>
2022-08-07 01:24:40 -0300

    Finalizando Tela de Captura do Qr Code

---
+++ frontend/LuxMeter/app/src/main/java/moe/feo/luxmeter/RoomDescription.java
@@ -0,0 +1,14 @@
+package moe.feo.luxmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_description);
+    }
+}
