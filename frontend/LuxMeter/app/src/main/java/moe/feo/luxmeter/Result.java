aa3e0790b580288a8a38a168dcdc2154451296e7
João Lucas Mota <joaomota59@hotmail.com>
2022-08-08 16:11:39 -0300

    add dependencias da api

--- frontend/LuxMeter/app/src/main/java/moe/feo/luxmeter/Result.java
+++ frontend/LuxMeter/app/src/main/java/moe/feo/luxmeter/Result.java
@@ -3,12 +3,17 @@ package moe.feo.luxmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
+
+    private TextView classificacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        classificacao = findViewById(R.id.infoText);
     }
 }
